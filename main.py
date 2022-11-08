442e31293bffe55a7a7e14d9b38283c5a461ccc6
EREN <EREN@DESKTOP-DRT5BB2>
2022-11-08 18:08:50 +0300

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
 x = requests.get('https://i.instagram.com/api/v1/users/web_profile_info/?username=soft.hubtr',headers={'x-ig-app-id': "936619743392459"})
-response = x.json()
-print(response["data"]["user"]["biography"])
+if x.status_code == 200:
+    response = x.json()
+    print(response["data"]["user"]["biography"])
